624d3de4dfa4a2a9192d077a6148cb8ab6a71ebd
Nguyen Quang Huy <nguyenquanghuyktd@gmail.com>
2025-06-10 23:23:16 +0700

    update ui2

--- public/script.js
+++ public/script.js
@@ -1,9 +1,16 @@
 const socket = io();
 
-socket.on('user-message', ({ senderId, message }) => {
-  const div = document.getElementById('messages');
-  div.innerHTML += `<p><strong>${senderId}:</strong> ${message}</p>`;
-  document.getElementById('senderId').value = senderId;
+socket.on('user-message', ({ senderId, message, userInfo }) => {
+  if (!contacts[senderId]) {
+    contacts[senderId] = { name: `${userInfo.first_name}`, avatar: userInfo.profile_pic, messages: [], info: userInfo };
+    addContactToSidebar(senderId);
+  }
+  contacts[senderId].messages.push({ from: 'user', text: message });
+
+  if (selectedUserId === senderId) {
+    renderMessages(senderId);
+    renderUserInfo(userInfo);
+  }
 });
 
 function sendReply() {
@@ -11,4 +18,13 @@ function sendReply() {
   const message = document.getElementById('reply').value;
   socket.emit('admin-reply', { senderId, message });
   document.getElementById('reply').value = '';
+}
+function renderUserInfo(user) {
+  const info = document.getElementById('user-info');
+  info.innerHTML = `
+    <img src="${user.profile_pic}" alt="avatar" class="img-thumbnail mb-2" width="80">
+    <p><strong>Name:</strong> ${user.first_name} ${user.last_name}</p>
+    <p><strong>Gender:</strong> ${user.gender}</p>
+    <p><strong>Locale:</strong> ${user.locale}</p>
+  `;
 }
--- webhook.js
+++ webhook.js
@@ -1,23 +1,33 @@
 const axios = require('axios');
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const getUserInfo = async (senderId) => {
+  const url = `https://graph.facebook.com/${senderId}?fields=first_name,last_name,profile_pic,locale,gender&access_token=${PAGE_ACCESS_TOKEN}`;
+  const res = await axios.get(url);
+  return res.data; // Trả về thông tin user
+};
 
 exports.handleWebhook = async (req, res, io) => {
   const body = req.body;
 
   if (body.object === 'page') {
-    body.entry.forEach(entry => {
+    for (const entry of body.entry) {
       const webhookEvent = entry.messaging[0];
       const senderId = webhookEvent.sender.id;
 
       if (webhookEvent.message && webhookEvent.message.text) {
         const message = webhookEvent.message.text;
 
-        // Gửi đến dashboard qua WebSocket
-        io.emit('user-message', { senderId, message });
-      }
-    });
+        const userInfo = await getUserInfo(senderId); // 👈 Lấy thông tin user
 
+        // Gửi message và info qua socket
+        io.emit('user-message', {
+          senderId,
+          message,
+          userInfo, // gửi kèm
+        });
+      }
+    }
     res.status(200).send('EVENT_RECEIVED');
   } else {
     res.sendStatus(404);
