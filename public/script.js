f71ecbce97826c81496cb2fedd86b5fc1ec6891b
Nguyen Quang Huy <nguyenquanghuyktd@gmail.com>
2025-06-11 00:23:00 +0700

    update ui v4

--- public/script.js
+++ public/script.js
@@ -23,8 +23,9 @@ function renderUserInfo(user) {
   const info = document.getElementById('user-info');
   info.innerHTML = `
     <img src="${user.profile_pic}" alt="avatar" class="img-thumbnail mb-2" width="80">
-    <p><strong>Name:</strong> ${user.first_name} ${user.last_name}</p>
+    <p><strong>Name:</strong> ${user.last_name} ${user.first_name} </p>
     <p><strong>Gender:</strong> ${user.gender}</p>
     <p><strong>Locale:</strong> ${user.locale}</p>
+    <p> <a href="https://facebook.com/${senderId}"><strong>Fackbook profile</strong></a></p>
   `;
 }
